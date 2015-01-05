10fc050feba3a74c7de6f1314ddddd394776349c
Cika <chatelcelia@gmail.com>
2015-01-05 19:03:13 +0100

    use equals method

--- tests/test.java
+++ tests/test.java
@@ -3,16 +3,14 @@ import org.junit.Test;
 
 public class test {
 	@Test
-	public void testMultiplication(){
+	public void testMultiplication() {
 		Dollar five = new Dollar(5);
-		Dollar product = five.times(2);
-		Assert.assertEquals(10,product.amount);
-		product = five.times(3);
-		Assert.assertEquals(15, product.amount);
+		Assert.assertEquals(new Dollar(10), five.times(2));
+		Assert.assertEquals(new Dollar(15), five.times(3));
 	}
 
 	@Test
-	public void testEquality(){
+	public void testEquality() {
 		Assert.assertTrue(new Dollar(5).equals(new Dollar(5)));
 		Assert.assertFalse(new Dollar(5).equals(new Dollar(6)));
 	}
