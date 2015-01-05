e38211e2369b90ac8d136c7b0b3c6e138cf22e08
Cika <chatelcelia@gmail.com>
2015-01-05 18:46:11 +0100

    multiplication returns object

---
+++ tests/test.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class test {
+	@Test
+	public void testMultiplication(){
+		Dollar five = new Dollar(5);
+		Dollar product = five.times(2);
+		Assert.assertEquals(10,product.amount);
+		product = five.times(3);
+		Assert.assertEquals(15, product.amount);
+	}
+}
