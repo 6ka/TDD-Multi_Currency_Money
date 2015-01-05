4e749b6e53d761de73115d28de63b6faeea53a2e
Cika <chatelcelia@gmail.com>
2015-01-05 19:06:29 +0100

    franc

--- tests/test.java
+++ tests/test.java
@@ -10,6 +10,13 @@ public class test {
 	}
 
 	@Test
+	public void testFrancMultiplication() {
+		Franc five = new Franc(5);
+		Assert.assertEquals(new Franc(10), five.times(2));
+		Assert.assertEquals(new Franc(15), five.times(3));
+	}
+
+	@Test
 	public void testEquality() {
 		Assert.assertTrue(new Dollar(5).equals(new Dollar(5)));
 		Assert.assertFalse(new Dollar(5).equals(new Dollar(6)));
